11dd3dec4346449d9bf341cf0582b61b252c1c9c
Cynthia Black <cindycblack@gmail.com>
2023-05-17 14:13:20 -0800

    Add hello world

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System; // need System.Console.WriteLine if not using System namespace
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // params in parens
+        }
+    }
+}
