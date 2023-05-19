dd67774e5f9cc1dee32578b31c9b823f7161df7d
Cynthia Black <cindycblack@gmail.com>
2023-05-18 16:04:53 -0800

    Add string interpolation to UserInput

--- UserInput/Program.cs
+++ UserInput/Program.cs
@@ -8,10 +8,10 @@ namespace UserInput
         {
             Console.WriteLine("Please enter your name.");
             string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine($"Hello {name}!");
             Console.WriteLine("How old are you?");
             int age = int.Parse(Console.ReadLine());
-            Console.WriteLine(name + " is " + age + " years old.");
+            Console.WriteLine($"{name} is {age} years old.");
         }
     }
 }
