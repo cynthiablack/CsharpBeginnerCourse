9a1675d72a849354d3346057f8cfca175718a977
Cynthia Black <cindycblack@gmail.com>
2023-05-19 16:37:58 -0800

    car constructor because why not

---
+++ ClassesIntro/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassesIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Car myCar = new Car("Georges");
+            Car anotherCar = new Car("Marlene");
+
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Accelerate();
+            myCar.Brake();
+            myCar.Accelerate();
+
+            anotherCar.Brake();
+        }
+    }
+
+    class Car
+    {
+        private int speed = 0;
+        private string name;
+
+        public Car(string carName)
+        {
+            name = carName;
+        }
+
+        public void Accelerate()
+        {
+            speed++;
+            showSpeed();
+        }
+        public void Brake()
+        {
+            speed--;
+            showSpeed();
+        }
+        private void showSpeed()
+        {
+            Console.WriteLine($"{name} is going {speed} kilometers per hour.");
+        }
+    }
+}
