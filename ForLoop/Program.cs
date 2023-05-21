2e71f8221b715feb458605184fadb466a8110a34
Cynthia Black <cindycblack@gmail.com>
2023-05-21 15:10:27 -0800

    Practicing for loops

---
+++ ForLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 10; i = i + 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 9; i >= 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            for (int i = 0; i <= 9; i++)
+            {
+                Console.Write($"row {i}: ");
+
+                for (int j = 0; j <= 9; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
