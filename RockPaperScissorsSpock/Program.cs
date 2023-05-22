3daaff602cac8006f8fb562b26f7df322d77caee
Cynthia Black <cindycblack@gmail.com>
2023-05-22 15:02:45 -0800

    Add do-while to rock paper scissors

--- RockPaperScissorsSpock/Program.cs
+++ RockPaperScissorsSpock/Program.cs
@@ -15,57 +15,83 @@ namespace RockPaperScissors
             string playerChoice;
             int playerValue = -1;
 
-            int computerValue = randomNumbers.Next(5);
-            string computerChoice;
+            do
+            {
+                int computerValue = randomNumbers.Next(3);
+                string computerChoice;
 
-            Console.Clear();
-            Console.Write("Please enter rock, paper, scissors, lizard, or Spock ");
-            playerChoice = Console.ReadLine().ToLower();
+                Console.Clear();
+                Console.Write("Please enter rock, paper, scissors, lizard, or Spock ");
+                playerChoice = Console.ReadLine().ToLower();
 
-            if (playerChoice.Equals("rock"))
-            {
-                playerValue = Rock;
-            }
-            else if (playerChoice.Equals("paper"))
-            {
-                playerValue = Paper;
-            }
-            else if (playerChoice.Equals("scissors"))
-            {
-                playerValue = Scissors;
-            }
-            else
-            {
-                Console.WriteLine($"{playerChoice} is not a valid choice.");
-            }
+                if (playerChoice.Equals("rock"))
+                {
+                    playerValue = Rock;
+                }
+                else if (playerChoice.Equals("paper"))
+                {
+                    playerValue = Paper;
+                }
+                else if (playerChoice.Equals("scissors"))
+                {
+                    playerValue = Scissors;
+                }
+                else
+                {
+                    Console.WriteLine($"{playerChoice} is not a valid choice.");
+                }
 
-            if (computerValue == Rock))
-            {
-                computerChoice = "rock";
-            }
-            else if (computerValue == Paper)
-            {
-                computerChoice = "paper";
-            }
-            else
-            {
-                computerChoice = "scissors";
-            }
+                if (computerValue == Rock))
+                {
+                    computerChoice = "rock";
+                }
+                else if (computerValue == Paper)
+                {
+                    computerChoice = "paper";
+                }
+                else
+                {
+                    computerChoice = "scissors";
+                }
 
-            Console.WriteLine($"Computer chose {computerChoice} and player chose {playerChoice}.");
-            Console.WriteLine($"Comparing {computerChoice} and {playerChoice}.");
+                Console.WriteLine($"Computer chose {computerChoice} and player chose {playerChoice}.");
+                Console.WriteLine($"Comparing {computerChoice} and {playerChoice}.");
 
-            if (playerValue == computerValue)
-            {
-                Console.WriteLine("It's a draw.");
+                if (playerValue == computerValue)
+                {
+                    Console.WriteLine("It's a draw.");
+                }
+                else if ((playerValue - 1 == computerValue) || (playerValue == Rock && computerValue == Scissors))
+                {
+                    Console.WriteLine("Player wins!");
+                }
+                else
+                {
+                    Console.WriteLine("Bummer - the machines win again.");
+                }
             }
-            else if ((playerValue - 1 == computerValue) || (playerValue == Rock && computerValue == Scissors))
+            while (GetYesOrNo("Would you like to play again?"));
+        }
+
+        /**
+        * Returns a boolean response to a yes/no question.
+        *
+        * @param string
+        *            The question to be asked.
+        * @return True if the answer was yes, False if no.
+        */
+        public static bool GetYesOrNo(string question)
+        {
+            char answer;
+            while (true)  // infinite loop.  return will exit the method, thus terminating the loop
             {
-                Console.WriteLine("Player wins!");
-            }
-            else
-            {
-                Console.WriteLine("Bummer - the machines win again.");
+                Console.Write($"{question} ");
+                answer = Console.ReadKey(true).KeyChar;
+                answer = char.ToLower(answer);
+                if (answer.Equals('y'))
+                    return true;
+                if (answer.Equals('n'))
+                    return false;
             }
         }
     }
